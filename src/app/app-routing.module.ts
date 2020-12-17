521b608019cdd647091a9a708c97a0613c4c0318
AlfianNI <72936082+AlfianNI@users.noreply.github.com>
2020-12-17 19:00:12 +0700

    tabs2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'tabs', pathMatch: 'full' },
   {
     path: 'home',
     loadChildren: () =>
