80c0ccd51690aed80abc3821ae1628ac907c5433
AlfianNI <72936082+AlfianNI@users.noreply.github.com>
2020-12-17 19:24:56 +0700

    Update login.page.ts

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -24,7 +24,7 @@ export class LoginPage implements OnInit {
       );
       this.authService.userId = userCredential.user.uid;
       await this.loginForm.hideLoading();
-      this.router.navigateByUrl('tabs');
+      this.router.navigateByUrl('tabs/profiles');
     } catch (error) {
       await this.loginForm.hideLoading();
       this.loginForm.handleError(error);
