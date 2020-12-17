98afe4ecd63ddae5ab53e89ce40eee086aad28c5
AlfianNI <72936082+AlfianNI@users.noreply.github.com>
2020-12-17 18:18:36 +0700

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,15 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCATun5z-FQAmxngFUWl0dkG2zmcn587B0',
-    authDomain: 'ionic-angular-event-manager.firebaseapp.com',
-    databaseURL: 'https://ionic-angular-event-manager.firebaseio.com',
-    projectId: 'ionic-angular-event-manager',
-    storageBucket: 'ionic-angular-event-manager.appspot.com',
-    messagingSenderId: '1055126399542',
-    appId: '1:1055126399542:web:3c36a5c070d8fd78'
+    apiKey: "AIzaSyDmEX2psN41hZoKrAzg8k5Sh16dJpJsxHQ",
+    authDomain: "uas22172-dd849.firebaseapp.com",
+    databaseURL: "https://uas22172-dd849-default-rtdb.firebaseio.com",
+    projectId: "uas22172-dd849",
+    storageBucket: "uas22172-dd849.appspot.com",
+    messagingSenderId: "821612395953",
+    appId: "1:821612395953:web:886cb025966d3fefb25190",
+    measurementId: "G-SGWW4DP0XD"
+
   }
 };
 
