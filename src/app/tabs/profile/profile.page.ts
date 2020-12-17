72b65ad4e99d5faaeec429675dccd0dc2d9f1a00
AlfianNI <72936082+AlfianNI@users.noreply.github.com>
2020-12-17 19:15:49 +0700

    a

--- src/app/tabs/profile/profile.page.ts
+++ src/app/tabs/profile/profile.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { UserProfile } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +12,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  public userProfile: UserProfile;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private profileService: ProfileService,
+    private alertCtrl: AlertController
+  ) {}
 
   ngOnInit() {
+    this.profileService.getUserProfile().then(profile$ => {
+      profile$.subscribe(userProfile => {
+        this.userProfile = userProfile;
+      });
+    });
+  }
+
+  async logOut(): Promise<void> {
+    await this.authService.logout();
+    this.router.navigateByUrl('login');
+  }
+
+  async updateName(): Promise<void> {
+    const alert = await this.alertCtrl.create({
+      subHeader: 'Your name',
+      inputs: [
+        {
+          type: 'text',
+          name: 'fullName',
+          placeholder: 'Your full name',
+          value: this.userProfile.fullName
+        }
+      ],
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: 'Save',
+          handler: data => {
+            this.profileService.updateName(data.fullName);
+          }
+        }
+      ]
+    });
+    return await alert.present();
+  }
+
+  async updateEmail(): Promise<void> {
+    const alert = await this.alertCtrl.create({
+      inputs: [
+        { type: 'text', name: 'newEmail', placeholder: 'Your new email' },
+        { name: 'password', placeholder: 'Your password', type: 'password' }
+      ],
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: 'Save',
+          handler: data => {
+            this.profileService
+              .updateEmail(data.newEmail, data.password)
+              .then(() => {
+                console.log('Email Changed Successfully');
+              })
+              .catch(error => {
+                console.log('ERROR: ' + error.message);
+              });
+          }
+        }
+      ]
+    });
+    return await alert.present();
+  }
+
+  async updatePassword(): Promise<void> {
+    const alert = await this.alertCtrl.create({
+      inputs: [
+        { name: 'newPassword', placeholder: 'New password', type: 'password' },
+        { name: 'oldPassword', placeholder: 'Old password', type: 'password' }
+      ],
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: 'Save',
+          handler: data => {
+            this.profileService.updatePassword(
+              data.newPassword,
+              data.oldPassword
+            );
+          }
+        }
+      ]
+    });
+    return await alert.present();
   }
 
 }
